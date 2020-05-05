93b6e813d6fb1d72dd15c43f82bce752e74e5591
Banyc <36535895+Banyc@users.noreply.github.com>
2020-05-06 02:41:58 +0800

    define projects

---
+++ src/MiniSQL.IndexManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniSQL.IndexManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/MiniSQL.Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniSQL.Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
