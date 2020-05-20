8140442f46b8c01be1f394b7cacd4b33d40167c3
Banyc <36535895+Banyc@users.noreply.github.com>
2020-05-20 13:58:08 +0800

    add `Ands` list in Expression

--- src/MiniSQL.Library/Models/Statement/Atom/AtomValue.cs
+++ src/MiniSQL.Library/Models/Statement/Atom/AtomValue.cs
@@ -5,5 +5,64 @@ namespace MiniSQL.Library.Models
         public int IntegerValue { get; set; } = 0;
         public string StringValue { get; set; } = "";
         public double FloatValue { get; set; } = 0.0;
+        public bool BooleanValue
+        {
+            get
+            {
+                switch (this.Type)
+                {
+                    case AttributeTypes.Char:
+                        if (this.StringValue == "")
+                            return false;
+                        else
+                            return true;
+                    case AttributeTypes.Int:
+                        if (this.IntegerValue == 0)
+                            return false;
+                        else
+                            return true;
+                    case AttributeTypes.Float:
+                        if (this.FloatValue == 0.0)
+                            return false;
+                        else
+                            return true;
+                    // workaround
+                    case AttributeTypes.Null:
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+            set
+            {
+                switch (this.Type)
+                {
+                    case AttributeTypes.Char:
+                        throw new System.InvalidCastException("bool could not cast to string");
+                    case AttributeTypes.Int:
+                        if (value)
+                            this.IntegerValue = 1;
+                        else
+                            this.IntegerValue = 0;
+                        break;
+                    case AttributeTypes.Float:
+                        if (value)
+                            this.FloatValue = 1;
+                        else
+                            this.FloatValue = 0;
+                        break;
+                    // if null, set it to int
+                    case AttributeTypes.Null:
+                        this.Type = AttributeTypes.Int;
+                        if (value)
+                            this.IntegerValue = 1;
+                        else
+                            this.IntegerValue = 0;
+                        break;
+                    default:
+                        throw new System.InvalidCastException("bool could not cast to unknown type");
+                }
+            }
+        }
     }
 }
--- src/MiniSQL.Library/Program.cs
+++ src/MiniSQL.Library/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using MiniSQL.Library.Models;
 
 namespace MiniSQL.Library
 {
@@ -6,7 +8,108 @@ namespace MiniSQL.Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start test!");
+
+            TestExpression();
+
+            Console.WriteLine("End test!");
+        }
+
+        private static void TestExpression()
+        {
+            TestAndList(); 
+            TestCalculateSingleValue();
+        }
+
+        private static void TestCalculateSingleValue()
+        {
+            Expression andsExpression = GetAndsExpression();
+            string attributeName = "b";
+            AtomValue attributeValue = new AtomValue();
+            attributeValue.Type = AttributeTypes.Char;
+            attributeValue.CharLimit = 10;
+            attributeValue.StringValue = "aaa";
+            Debug.Assert(andsExpression.CheckKey(attributeName, attributeValue) == true);
+        }
+
+        private static void TestAndList()
+        {
+            Expression andsExpression = GetAndsExpression();
+            var andList = andsExpression.Ands;
+            // "not equal" operator is ignored
+            Debug.Assert(andList.ContainsKey("a") == false);
+            Debug.Assert(andList["b"].Operator == Operator.LessThanOrEqualTo);
+            Debug.Assert(andList["b"].RightOperant.ConcreteValue.StringValue == "str");
+            Debug.Assert(andList["c"].Operator == Operator.LessThan);
+            Debug.Assert(andList["c"].RightOperant.ConcreteValue.FloatValue == 6.6);
+        }
+
+        private static Expression GetAndsExpression()
+        {
+            // __tree structure__
+            //             and 1
+            //        and 2,     > 3
+            //     != 4, <= 5, 6.6 6, c 7
+            // 32 8, a, 9, b 10, "str" 11
+
+            // 32
+            Expression node8 = new Expression();
+            node8.Operator = Operator.AtomConcreteValue;
+            node8.ConcreteValue = new AtomValue();
+            node8.ConcreteValue.Type = AttributeTypes.Int;
+            node8.ConcreteValue.IntegerValue = 32;
+            // a
+            Expression node9 = new Expression();
+            node9.Operator = Operator.AtomVariable;
+            node9.AttributeName = "a";
+            // b
+            Expression node10 = new Expression();
+            node10.Operator = Operator.AtomVariable;
+            node10.AttributeName = "b";
+            // "str"
+            Expression node11 = new Expression();
+            node11.Operator = Operator.AtomConcreteValue;
+            node11.ConcreteValue = new AtomValue();
+            node11.ConcreteValue.Type = AttributeTypes.Char;
+            node11.ConcreteValue.CharLimit = 5;
+            node11.ConcreteValue.StringValue = "str";
+            // !=
+            Expression node4 = new Expression();
+            node4.Operator = Operator.NotEqual;
+            node4.LeftOperant = node8;
+            node4.RightOperant = node9;
+            // <=
+            Expression node5 = new Expression();
+            node5.Operator = Operator.LessThanOrEqualTo;
+            node5.LeftOperant = node10;
+            node5.RightOperant = node11;
+            // 6.6
+            Expression node6 = new Expression();
+            node6.Operator = Operator.AtomConcreteValue;
+            node6.ConcreteValue = new AtomValue();
+            node6.ConcreteValue.Type = AttributeTypes.Float;
+            node6.ConcreteValue.FloatValue = 6.6;
+            // c
+            Expression node7 = new Expression();
+            node7.Operator = Operator.AtomVariable;
+            node7.AttributeName = "c";
+            // and
+            Expression node2 = new Expression();
+            node2.Operator = Operator.And;
+            node2.LeftOperant = node4;
+            node2.RightOperant = node5;
+            // >
+            Expression node3 = new Expression();
+            node3.Operator = Operator.MoreThan;
+            node3.LeftOperant = node6;
+            node3.RightOperant = node7;
+            // and
+            Expression node1 = new Expression();
+            node1.Operator = Operator.And;
+            node1.LeftOperant = node2;
+            node1.RightOperant = node3;
+
+            return node1;
         }
     }
 }
