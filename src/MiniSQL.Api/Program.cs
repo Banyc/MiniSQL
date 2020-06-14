1b2450f5a4c9f266c2392dd9ece00806d3a12968
Banyc <36535895+Banyc@users.noreply.github.com>
2020-06-14 10:27:07 +0800

    fix table alignment

--- src/MiniSQL.Api/Program.cs
+++ src/MiniSQL.Api/Program.cs
@@ -19,26 +19,24 @@ namespace MiniSQL.Api
         // TODO: view of the whole solution 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // print prologue
+            Console.WriteLine();
+            Console.WriteLine("Hello MiniSQL!");
+            Console.WriteLine();
+            // init
             string dbPath = "./testdbfile.minidb";
-            // File.Delete(dbPath);
             Pager pager = new Pager(dbPath);
             FreeList freeList = new FreeList(pager);
             IBufferManager bTreeController = new BTreeController(pager, freeList);
-
             Stopwatch stopwatch = new Stopwatch();
-
             IInterpreter interpreter = new Parsing();
             ICatalogManager catalogManager = new Catalog();
             // IIndexManager indexManager = new IndexManager();
             IRecordManager recordManager = new RecordContext(pager, bTreeController);
-
             IApi api = new Api(interpreter, catalogManager, recordManager);
-
+            // read input
             StringBuilder input = new StringBuilder();
-
             bool isExit = false;
-
             while (!isExit)
             {
                 // read input
@@ -75,45 +73,92 @@ namespace MiniSQL.Api
                 // clear input to the interpreter
                 input.Clear();
             }
-
+            // save database file
             pager.Close();
         }
 
         private static void PrintRows(SelectResult result)
         {
+            // get size of each column
+            List<int> sizes = GetColumnSize(result);
+            // change color
             ConsoleColor defaultColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Magenta;
             // print names
+            int columnIndex = 0;
             foreach (AttributeDeclaration name in result.ColumnDeclarations)
             {
-                Console.Write($"{name.AttributeName}\t|\t");
+                string format = "{0, " + (-sizes[columnIndex]).ToString() + "}";
+                Console.Write($" {string.Format(format, name.AttributeName)} |");
+                columnIndex++;
             }
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Cyan;
             // print rows
             foreach (List<AtomValue> row in result.Rows)
             {
+                columnIndex = 0;
                 foreach (AtomValue value in row)
                 {
+                    string format = "{0, " + sizes[columnIndex].ToString() + "}";
+                    string stringToPrint = "";
                     switch (value.Type)
                     {
                         case AttributeTypes.Int:
-                            Console.Write($"{value.IntegerValue}\t|\t");
+                            stringToPrint = $"{value.IntegerValue}";
                             break;
                         case AttributeTypes.Char:
-                            Console.Write($"\"{value.StringValue}\"\t|\t");
+                            stringToPrint = $"\"{value.StringValue}\"";
                             break;
                         case AttributeTypes.Float:
-                            Console.Write($"{value.FloatValue.ToString("0.##")}\t|\t");
+                            stringToPrint = $"{value.FloatValue.ToString("0.##")}";
                             break;
                         case AttributeTypes.Null:
-                            Console.Write($"NULL\t|\t");
+                            stringToPrint = $"NULL";
                             break;
                     }
+                    Console.Write($" {string.Format(format, stringToPrint)} |");
+                    columnIndex++;
                 }
                 Console.WriteLine();
             }
             Console.ForegroundColor = defaultColor;
+        }
+
+        private static List<int> GetColumnSize(SelectResult result)
+        {
+            List<int> sizes = new List<int>();
+            foreach (AttributeDeclaration name in result.ColumnDeclarations)
+            {
+                sizes.Add(name.AttributeName.Length);
+            }
+            foreach (List<AtomValue> row in result.Rows)
+            {
+                int columnIndex = 0;
+                foreach (AtomValue value in row)
+                {
+                    int size = 0;
+                    switch (value.Type)
+                    {
+                        case AttributeTypes.Int:
+                            size = value.IntegerValue.ToString().Length;
+                            break;
+                        case AttributeTypes.Char:
+                            size = $"\"{value.StringValue}\"".Length;
+                            break;
+                        case AttributeTypes.Float:
+                            size = value.FloatValue.ToString("0.##").Length;
+                            break;
+                        case AttributeTypes.Null:
+                            size = "NULL".Length;
+                            break;
+                    }
+                    if (sizes[columnIndex] < size)
+                        sizes[columnIndex] = size;
+                    columnIndex++;
+                }
+            }
+            return sizes;
         }
     }
 }
