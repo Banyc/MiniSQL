1890ef6a876fddbc4e1b00b0197fdabf2a994fb7
Banyc <36535895+Banyc@users.noreply.github.com>
2020-07-30 16:18:30 +0800

    enhance readability

--- src/MiniSQL.IndexManager/Interfaces/IIndexManager.cs
+++ src/MiniSQL.IndexManager/Interfaces/IIndexManager.cs
@@ -1,25 +1,59 @@
 using MiniSQL.Library.Models;
 using MiniSQL.IndexManager.Models;
 using System.Collections.Generic;
-using System;
 
 namespace MiniSQL.IndexManager.Interfaces
 {
     public interface IIndexManager
     {
-        // when create table
+        /// <summary>
+        /// use when creating table
+        /// </summary>
+        /// <returns>The root node of the new B+ tree</returns>
         BTreeNode OccupyNewTableNode();
-        // when drop table
+        /// <summary>
+        /// use when dropping table
+        /// </summary>
+        /// <param name="root">root node of the B+ Tree</param>
         void RemoveTree(BTreeNode root);
-        // insert cell
-        BTreeNode InsertCell(BTreeNode Root, DBRecord key, DBRecord dBRecord);
-        // delete cell(s) that satisfy `condition`
-        // `keyName` := primary key in table tree; indexed value in index tree
+        /// <summary>
+        /// insert a row/record/cell
+        /// </summary>
+        /// <param name="root">the root of the B+ tree</param>
+        /// <param name="key">primary key in table tree and indexed column in index tree</param>
+        /// <param name="dBRecord">new row of values to insert</param>
+        /// <returns>new root node of the B+ tree</returns>
+        BTreeNode InsertCell(BTreeNode root, DBRecord key, DBRecord dBRecord);
+        /// <summary>
+        /// delete cell(s) that satisfy `condition`
+        /// </summary>
+        /// <param name="root">the root of the B+ tree</param>
+        /// <param name="condition">condition to satisfy</param>
+        /// <param name="keyName">primary key in table tree; indexed value in index tree</param>
+        /// <param name="attributeDeclarations">the names of the columns</param>
+        /// <returns>new root node of the B+ tree</returns>
         BTreeNode DeleteCells(BTreeNode root, Expression condition, string keyName, List<AttributeDeclaration> attributeDeclarations);
-        // return matches that satisfy `condition`
-        // `keyName` := primary key in table tree; indexed value in index tree
+        /// <summary>
+        /// return matches that satisfy `condition`
+        /// </summary>
+        /// <param name="root">the root of the B+ tree</param>
+        /// <param name="condition">condition to satisfy</param>
+        /// <param name="keyName">primary key in table tree; indexed value in index tree</param>
+        /// <param name="attributeDeclarations">the names of the columns</param>
+        /// <returns>matches that satisfy `condition`</returns>
         List<BTreeCell> FindCells(BTreeNode root, Expression condition, string keyName, List<AttributeDeclaration> attributeDeclarations);
+        /// <summary>
+        /// find a row/record/cell by the key value
+        /// </summary>
+        /// <param name="key">primary key in table tree; indexed value in index tree</param>
+        /// <param name="root">the root of the B+ tree</param>
+        /// <returns>the matched cell</returns>
         BTreeCell FindCell(DBRecord key, BTreeNode root);
-        System.Collections.Generic.IEnumerable<BTreeCell> LinearSearch(BTreeNode root);
+        /// <summary>
+        /// enumerate all leaf nodes of the B+ tree
+        /// </summary>
+        /// <param name="root">the root of the B+ tree</param>
+        /// <returns>each leaf node</returns>
+        IEnumerable<BTreeCell> LinearSearch(BTreeNode root);
     }
 }
